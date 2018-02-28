6373cb04eac7a1567e3c3a1f8128aaddf01e9199
danielcampos <ddregalo@gmail.com>
2018-02-28 23:46:57 +0000

    Add journey class and spec file - test passed

---
+++ lib/journey.rb
@@ -0,0 +1,21 @@
+class Journey
+
+
+
+
+end
+# def start_journey
+# end
+#
+# # def finish_journey
+# # end
+# #
+# # def fare
+# # end
+# #
+# # def complete?
+# # end
+# starting a journey,
+# finishing a journey,
+# calculating the fare of a journey,
+# returning whether or not the journey is complete
