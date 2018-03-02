a85c663fa9450fe06a61d45b6be24d78591a6e55
danielcampos <ddregalo@gmail.com>
2018-03-02 14:00:56 +0000

    Add test to initialize a tube_map array with zones - passed

--- lib/station.rb
+++ lib/station.rb
@@ -3,9 +3,9 @@ class Station
 
   def initialize
     @tube_map = [
-    {:name => "Clapham", :zone => 3}
-    {:name => "Stockwell", :zone => 3}
-    {:name => "Bank", :zone => 2}
+    {:name => "Clapham", :zone => 3},
+    {:name => "Stockwell", :zone => 3},
+    {:name => "Bank", :zone => 2},
     {:name => "Old Street", :zone => 1}
     ]
   end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -9,4 +9,13 @@ describe Station do
   it 'Should create zone instance variable' do
     expect(subject.zone).to eq nil
   end
+
+  it 'Should initialize tube_map array' do
+    expect(subject.tube_map).to eq([
+    {:name => "Clapham", :zone => 3},
+    {:name => "Stockwell", :zone => 3},
+    {:name => "Bank", :zone => 2},
+    {:name => "Old Street", :zone => 1}
+    ])
+  end
 end
